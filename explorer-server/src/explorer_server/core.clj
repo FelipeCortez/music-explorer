287d106464baa5b6ff523866bd85f21d8296fd05
FelipeCortez <felipecortezfi@gmail.com>
2020-02-08 13:33:57 -0300

    Simple query with sh/sh and conversion to edn

--- explorer-server/src/explorer_server/core.clj
+++ explorer-server/src/explorer_server/core.clj
@@ -1,5 +1,19 @@
 (ns exporer-server.core
   (:require [clojure.java.shell :as sh]))
 
-;; (sh/sh "beet" "ls" "-a" "added+" "'added:2020'" "-f" "'$added $albumartist - $album'")
-(println (:out (sh/sh "beet" "ls" "-a" "mars")))
+(def fields [:added :albumartist :album :country :path])
+
+(defn fields->field-str [fields]
+  (->> fields
+       (map (fn [s] (str "$" (name s))))
+       (clojure.string/join "\t")))
+
+(fields->field-str fields)
+
+(defn sh-result->m [sh-result]
+  (->> sh-result
+       clojure.string/split-lines
+       (map (fn [s] (clojure.string/split s #"\t")))
+       (map (fn [coll] (zipmap fields coll)))))
+
+(sh-result->m (:out (sh/sh "beet" "ls" "-a" "added:2020" "-f" (fields->field-str fields))))
