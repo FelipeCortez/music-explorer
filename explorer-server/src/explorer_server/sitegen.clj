039afe07fb808d3e20ba4e627d7eb65571dca7c7
FelipeCortez <felipecortezfi@gmail.com>
2020-08-23 10:35:26 -0300

    Add helper functions for a timeline widget

--- explorer-server/src/explorer_server/sitegen.clj
+++ explorer-server/src/explorer_server/sitegen.clj
@@ -1,5 +1,6 @@
 (ns explorer-server.sitegen
   (:require [explorer-server.core :as data]
+            [tick.alpha.api :as t]
             [hiccup.core :refer [html]]
             [hiccup.page :refer [html5 doctype include-css include-js]]))
 
@@ -38,18 +39,55 @@
               (head title description js css)
               (body content)])))
 
-(comment
-  (let [albums-hiccup (for [album (data/last-added)]
-                        [:li [:div
-                              [:img {:src (str "http://127.0.0.1:8002" (:albums/artpath album))
-                                     :width "150px"}]
-                              [:h1 (str (:albums/albumartist album)
-                                        " · "
-                                        (:albums/album album))]
-                              [:h2 (:albums/original_year album)]
-                              [:hr]]])]
+(defn decades [first last]
+  (loop [coll    []
+         current first]
+    (if (< current last)
+      (recur (conj coll current)
+             (+ current (- 10 (mod current 10))))
+      (conj coll last))))
 
-    (spit "/home/felipecortez/Dev/music-explorer/explorer-web/index.html"
-          (document {:title       "Music Explorer"
-                     :description "A music explorer"}
-                    [:ul albums-hiccup]))))
+(defn year-to-pixel [first-year last-year
+                     first-coord last-coord
+                     year]
+  (let [normalized-last (- last-coord first-coord)]
+    normalized-last))
+
+(year-to-pixel 1945 2020 16 800 1955)
+
+(decades 1945 2020)
+
+(defn timeline []
+  (let [initial-year 1945 ;; https://en.wikipedia.org/wiki/History_of_sound_recording#The_Magnetic_Era_(1945_to_1975)
+        end-year     (t/int (t/year (t/now)))]
+    [:svg {:viewBox "0 0 840 360"
+           :width   "840px"
+           :height  "360px"
+           :xlmns   "http://www.w3.org/2000/svg"}
+     [:line {:x1     16.5 :x2 16.5
+             :y1     0    :y2 300
+             :stroke "black"}]
+     [:text {:transform "translate(16, 320) rotate(45)"}
+      initial-year]
+     [:line {:x1     800.5 :x2 800.5
+             :y1     0    :y2 300
+             :stroke "black"}]
+     [:text {:transform "translate(800, 320) rotate(45)"}
+      end-year]]))
+
+(let [albums-hiccup (for [album (data/last-added)]
+                      [:li [:div
+                            [:img {:src (str "http://127.0.0.1:8002" (:albums/artpath album))
+                                   :width "150px"}]
+                            [:h1 (str (:albums/albumartist album)
+                                      " · "
+                                      (:albums/album album))]
+                            [:h2 (:albums/original_year album)]
+                            [:hr]]])]
+
+  (spit "/home/felipecortez/Dev/music-explorer/explorer-web/index.html"
+        (document {:title       "Music Explorer"
+                   :description "A music explorer"}
+                  (list
+                   (timeline)
+                   [:ul albums-hiccup]))))
